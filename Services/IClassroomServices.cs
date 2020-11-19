f8cfe04455a4d22a02736d1dbfd088c2d57347b1
Eric <evandenoutenaar@gmail.com>
2020-11-19 15:51:44 +0100

    added services

---
+++ Services/IClassroomServices.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ClassroomApp.Services
+{
+    public interface IClassroomService
+    {
+        bool AddStudent(Student student);
+
+        IEnumerable<Student> GetAllStudents();
+
+    }
+}
